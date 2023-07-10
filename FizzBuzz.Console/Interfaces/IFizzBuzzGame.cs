738d888fbcfe2152c0bd23547f569549265f39ea
Stephen Stewart <stephen.stewart@paxton-access.co.uk>
2023-07-10 18:49:52 +0100

    Interface Creation

---
+++ FizzBuzz.Console/Interfaces/IFizzBuzzGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Console.Interfaces
+{
+    public interface IFizzBuzzGame
+    {
+        int RangeStartNumber { get; set; }
+        int RangeEndNumber { get; set; }
+        IFizzBuzzRule[] Rules { get; set; }
+
+        List<string> GetRangeOutput();
+    }
+}
