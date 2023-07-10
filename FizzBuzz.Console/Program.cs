ffb70c2161545844cb350e516ddabcc957883330
Stephen Stewart <stephen.stewart@paxton-access.co.uk>
2023-07-10 20:22:00 +0100

    Added the console app to show output on console

--- FizzBuzz.Console/Program.cs
+++ FizzBuzz.Console/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzz.Console.Interfaces;
+using FizzBuzz.Console.Types;
+using FizzBuzz.Console.Types.Rules;
+
+Console.WriteLine("***FizzBuzz Game***");
+int rangeStartNumber, rangeEndNumber;
+
+while (true)
+{
+    Console.Write("Enter the range start number: ");
+    var rangeStartInput = Console.ReadLine();
+    if (int.TryParse(rangeStartInput, out rangeStartNumber))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid start number. Please enter a valid integer.");
+    }
+}
+
+while (true)
+{
+    Console.Write("Enter the range end number: ");
+    var rangeEndInput = Console.ReadLine();
+    if (int.TryParse(rangeEndInput, out rangeEndNumber))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid end number. Please enter a valid integer.");
+    }
+}
+
+var fizzBuzzRules = new IFizzBuzzRule[] {
+    new FizzBuzzRule(),
+    new FizzRule(),
+    new BuzzRule(),
+    new DefaultRule()
+};
+
+var fizzBuzzGame = new FizzBuzzGame(
+    rangeStartNumber,
+    rangeEndNumber,
+    fizzBuzzRules
+);
+var fizzBuzzGameOutputs = fizzBuzzGame.GetRangeOutput();
+
+PrintFizzBuzzGameOutputs(fizzBuzzGameOutputs);
+
+
+
+
+static void PrintFizzBuzzGameOutputs(List<string> fizzBuzzGameOutputs)
+{
+    Console.WriteLine("Game Output:");
+    for (int i = 0; i < fizzBuzzGameOutputs.Count; i++)
+    {
+        if (i != 0 && i % 5 == 0) System.Console.WriteLine();
+
+        System.Console.Write(fizzBuzzGameOutputs[i] + " ");
+    }
+
+    Console.WriteLine();
+}
